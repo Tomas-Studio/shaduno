a595685516dbbd7dcb9c869ff5b7319bf4931560
olakunle.olatunji <tjomas20@gmail.com>
2024-05-10 18:13:03 +0100

    feat: added shadcn nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
   eslint: {
     config: {
       stylistic: true,
