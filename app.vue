2a26d2582be1ff24fa6ad9397c60f37c2dfb8981
olakunle.olatunji <tjomas20@gmail.com>
2024-05-13 03:33:18 +0100

    feat: added and configured shadcn to work unocss, ui added, Button & Alert

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+import '@unocss/reset/tailwind.css'
+
+useHead({
+  htmlAttrs: { lang: 'en' },
+  bodyAttrs: { class: 'font-default' },
+  link: [{ rel: 'icon', href: '/northbase.png' }],
+})
+
+useSeoMeta({
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  title: 'Unshade',
+  description:
+    'Unshade',
+  ogImage: '/social-image.png',
+  twitterImage: '/social-image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div p4>
+    <Button>
+      Click to Open
+    </Button>
+
+    <Alert mt>
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,8 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
+import presetAnimations from 'unocss-preset-animations'
+import { presetShadcn } from 'unocss-preset-shadcn'
 
 export default defineConfig({
   shortcuts: [
@@ -30,14 +32,27 @@ export default defineConfig({
         mono: 'DM Mono:300,400',
         sans: 'Inter:300,400,500,600,700',
         default: [
-          { name: 'Pally', weights: ['400', '500', '600', '700'] },
+          { name: 'Pally', weights: ['300', '400', '500', '600', '700'] },
           { name: 'sans-serif', provider: 'none' },
         ],
       },
     }),
+    presetAnimations(),
+    presetShadcn({ color: 'blue' }),
   ],
   transformers: [
     transformerDirectives(),
     transformerVariantGroup(),
   ],
+  // content: {
+  //   pipeline: {
+  //     include: [
+  //       // the default
+  //       /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+  //       // include js/ts files
+  //       "./**/*.{js,ts}",
+  //     ],
+  //   },
+  // },
+  include: [/\.ts/, /\.vue$/, /\.vue\?vue/],
 })
